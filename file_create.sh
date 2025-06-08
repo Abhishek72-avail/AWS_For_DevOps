eecc13c89d6b330de96e2b8b8694556659bb5133
abhishek <abhishekprasad7250@gmail.com>
2025-06-08 13:23:53 +0530

    add file

---
+++ file_create.sh
@@ -0,0 +1,40 @@
+for i in {11..30}; do
+  touch "day$i/day$i.md"
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+# # Starting from day11 to day30
+# for i in {11..30}
+# do
+#     folder="day$i"
+#     file="day$i.md"
+
+#     # Folder create karo
+#     mkdir -p "$folder"
+
+#     # Us folder ke andar .md file create karo
+#     touch "$folder/$file"
+# done
+
+# echo "Folders aur files create ho gaye!"
---
+++ remove.sh
@@ -0,0 +1,3 @@
+#  for i in {1..10}; do
+# #   rm -r "day$i"
+# # done
